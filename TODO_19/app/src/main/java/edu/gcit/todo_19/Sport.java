3414d1efa278c16eebee100c72ff00bb8a226582
kezang-jpg <kezangd574@gmail.com>
2021-05-18 23:26:46 +0600

    Todo_19

---
+++ TODO_19/app/src/main/java/edu/gcit/todo_19/Sport.java
@@ -0,0 +1,24 @@
+package edu.gcit.todo_19;
+
+public class Sport {
+    private String title, info;
+    private final int imageResource;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getInfo() {
+        return info;
+    }
+
+    public int getImageResource() {
+        return imageResource;
+    }
+
+    public Sport(String title, String info, int imageResource){
+        this.title = title;
+        this.info = info;
+        this.imageResource = imageResource;
+    }
+}
